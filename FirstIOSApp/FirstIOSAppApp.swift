d04c20f6438de1d1bf9af82ff01eb2d24058c148
Satskill <ahmetthit@hotmail.com>
2025-10-15 15:26:54 +0300

    Initial Commit

---
+++ FirstIOSApp/FirstIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIOSAppApp.swift
+//  FirstIOSApp
+//
+//  Created by admin on 15.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirstIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
